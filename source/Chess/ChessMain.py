4c3b1313e43a91fe0d7ae74db2dfbbc4cac86023
Karolina Kleciak <karolinakleciak8@gmail.com>
2024-02-17 15:32:12 +0100

    Task #009 - Pawn promotion and en passant

--- source/Chess/ChessMain.py
+++ source/Chess/ChessMain.py
@@ -59,13 +59,14 @@ def main():
                 if len(playerClicks) == 2:  # after 2nd click
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    if move in validMoves:
-                        gs.makeMove(move)
-                        moveMade = True
-                    sqSelected = ()  # reset user clicks
-                    playerClicks = []
-                else:
-                    playerClicks = [sqSelected]
+                    for i in range(len(validMoves)):
+                        if move == validMoves[i]:
+                            gs.makeMove(validMoves[i])
+                            moveMade = True
+                            sqSelected = ()  # reset user clicks
+                            playerClicks = []
+                    if not moveMade:
+                        playerClicks = [sqSelected]
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z:
                     gs.undoMove()
